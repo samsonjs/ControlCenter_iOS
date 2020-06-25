306d775027c77d459e1f5592d357def40b3d914d
Sami Samhuri <sami@samhuri.net>
2020-06-25 07:41:59 -0700

    Initial Commit

---
+++ ControlCenter_iOS/ControlCenter_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlCenter_iOSApp.swift
+//  ControlCenter_iOS
+//
+//  Created by Sami Samhuri on 2020-06-25.
+//
+
+import SwiftUI
+
+@main
+struct ControlCenter_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
